838ff4a31f39b90812382b939f458c5ad4c80b84
kama <116267218+kama93i@users.noreply.github.com>
2026-02-25 12:40:35 +0100

    Fixed error messages

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ pub enum AppError {
     #[error("Connection {0}:{1} refused")]
     ConnectionError(String, u16),
 
-    #[error("Invalid input {0}")]
+    #[error("Invalid Input: {0}")]
     InputError(String),
 }
 
